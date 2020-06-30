4e239b670238a2b11163328e6aeca50e3619d9c8
Divine Ugizwenayo <ugizwenayodiny@gmail.com>
2020-06-30 01:24:52 -1000

    (feat): routing burger project
    - create checkout folder with checkout file
    - create checkoutSummary folder with its files

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const checkoutSummary = (props) =>{
+    return(
+        <div className={classes.CheckoutSummary}>
+            <h1>Hope you enjoy your burger!</h1>
+            <div style={{width:'100%',margin:'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+                <Button btnType='Success'>CONTINUE</Button>
+                <Button btnType='Danger'>CANCEL</Button>
+            </div>
+        </div>
+    )
+}
+
+export default checkoutSummary;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-
+import {Route,Switch} from 'react-router-dom';
 import Layout from '../hoc/Layout/Layout';
 import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder></BurgerBuilder>
+          <Switch>
+            <Route path='/' exact component={BurgerBuilder}></Route>
+            <Route path='/checkout' component={Checkout}></Route>
+          </Switch>
         </Layout>
       </div>
     );
